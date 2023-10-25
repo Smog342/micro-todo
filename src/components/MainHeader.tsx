b5fff58779776e923315b32fb80952bef7e7631d
Smog342 <mitro_02@mail.ru>
2023-10-25 09:39:10 +0300

    slight header changes

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,6 +1,6 @@
-import { Badge, Button, Input } from "antd";
+import { Avatar, Badge, Button, Input } from "antd";
 import {Header } from "antd/es/layout/layout";
-import {NotificationOutlined, QuestionOutlined,SettingOutlined} from "@ant-design/icons";
+import {HolderOutlined, NotificationOutlined, QuestionOutlined,SettingOutlined, UserOutlined, WindowsOutlined} from "@ant-design/icons";
 
 const {Search} = Input;
 
@@ -8,8 +8,10 @@ function MainHeader(){
 
     return(
         <Header className="bg-blue-600 p-0 flex justify-between items-center sticky">
-        <div className="pl-3">Icon</div>
-        <div>To Do</div>
+        <Button type="text" className="hover:bg-blue-700">
+            <WindowsOutlined className="text-xl"></WindowsOutlined>
+        </Button>
+        <div className="text-xl font-bold">To Do</div>
         <div className="flex items-center w-2/5">
             <Search>
             </Search>
@@ -27,7 +29,7 @@ function MainHeader(){
             </Badge>
             </Button>
         </div>
-        <div className="pr-3">Avatar</div>
+        <Avatar className="mr-3"><UserOutlined className="text-xl"></UserOutlined></Avatar>
         </Header>
     );
 
